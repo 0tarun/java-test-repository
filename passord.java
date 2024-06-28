54bd686b70039df59fc239c2e3f2bb1e0d65a8ac
0tarun <tdas221498@bscse.uiu.ac.bd>
2024-06-28 20:13:18 +0600

    this is my first change

--- passord.java
+++ passord.java
@@ -2,6 +2,7 @@ public class passord {
     public void view()
     {
         System.out.print("Hi this is my password ");
+        System.out.println("This one of the secure password");
     }
     
     
